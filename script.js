397f0559363adf5029f48e16d92c8db16e787a6e
Bernardo Silva <bernardofasilva@protonmail.com>
2025-03-06 19:28:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const setWeather = (city, min, max) => {
+  document.getElementById("weather").textContent = "Estão entre " +
+    min.toString() + " °C e " + max.toString() + " °C em " + city.toString();
+};
+
+const populateCities = (options) => {
+  const selectElement = document.getElementById("cities");
+  selectElement.innerHTML = "";
+  options.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option;
+    optionElement.textContent = option;
+    selectElement.appendChild(optionElement);
+  });
+
+  selectElement.addEventListener("change", function () {
+    fetchWeather(this.value);
+  });
+};
+
+var citiesMap;
+
+const fetchCities = async () => {
+  const API_URL = "https://api.ipma.pt/open-data/distrits-islands.json";
+  const response = await fetch(API_URL).then((r) => r.json());
+  citiesMap = Object.fromEntries(
+    response["data"].map((c) => [c["local"], c["globalIdLocal"]]),
+  );
+  populateCities(Object.keys(citiesMap));
+};
+
+const fetchWeather = async (city) => {
+  const url =
+    "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/" +
+    citiesMap[city].toString() + ".json";
+  const response = await fetch(url).then((r) => r.json());
+  const weather = response["data"][0];
+  setWeather(city, weather["tMin"], weather["tMax"]);
+};
+
+const main = async () => {
+  await fetchCities();
+  await fetchWeather("Aveiro");
+};
+
+main();
